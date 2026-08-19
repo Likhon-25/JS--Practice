22b4b89f716fbd0aecf9f9648356d35c22550e22
Likhon-25 <likhonmondol327@gmail.com>
2026-08-20 02:12:18 +0600

    ES6 practice p2

---
+++ Module 16 practice task/p2.js
@@ -0,0 +1,10 @@
+const bookTicket = (movie, seats = 1, pricePerSeat = 300) => {
+  if (typeof movie !== "string" || seats < 0 || pricePerSeat < 0) {
+    return "Invalid";
+  }
+  const totalPrice = seats * pricePerSeat;
+  const output = `${movie}: ${seats} seat(s), Total ৳${totalPrice}`;
+  return output;
+};
+
+console.log(bookTicket("Movie", 0, 500));
